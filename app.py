6721cfbc715cf2adac1ae001a0f5dad0240ee76b
evmaki <e.king@utexas.edu>
2022-10-18 19:15:57 -0500

    Correct bugs in app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_url_path='', static_folder='ui/build/')
@@ -7,4 +8,4 @@ def index():
     return send_from_directory('ui/build/', 'index.html')
 
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
